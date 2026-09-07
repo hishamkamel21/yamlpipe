7c787ecbbe576f884f696c7b8c39de01419cfbbe
hisham21 <hishamkamel636@gmail.com>
2026-09-07 17:01:40 +0300

    ddddddddddddd

--- core/template_manager.py
+++ core/template_manager.py
@@ -1,7 +1,7 @@
 import copy
 import logging
 from typing import Any, Dict, List
-from yamlpipe.core.getter import Getter
+from yamlpipe.getter import Getter
 from yamlpipe.core.vars_manager import VariablesManager
 
 logger = logging.getLogger("TemplateManager")
