b89e566c0ef011d8cd1253e40fb7c1e7ced2d341
hisham21 <hishamkamel636@gmail.com>
2026-09-07 16:19:43 +0300

    temp....6

--- core/template_manager.py
+++ core/template_manager.py
@@ -3,7 +3,7 @@
 import copy
 import logging
 from typing import Any, Dict, List
-from yamlpipe.core.getter import Getter
+from yamlpipe.getter import Getter
 from yamlpipe.core.vars_manager import VariablesManager
 from yamlpipe.utility.placeholder_resolver import TemplateResolver
 
